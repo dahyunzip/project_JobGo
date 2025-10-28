01af73af1367c21c6e0fd6176b9ad1a6412e3bb4
gnlau <gnlau@121.175.193.81>
2025-10-29 00:06:03 +0900

    YHM) 중간 작업

--- JobGo/src/main/java/com/itwillbs/controller/ComBoardController.java
+++ JobGo/src/main/java/com/itwillbs/controller/ComBoardController.java
@@ -1,9 +1,14 @@
 package com.itwillbs.controller;
 
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.itwillbs.service.ComBoardService;
 
 @Controller
 @RequestMapping("/comboard/*")
@@ -12,6 +17,17 @@ public class ComBoardController {
 	private static final Logger logger 
 		= LoggerFactory.getLogger(ComBoardController.class);
 	
+	@Inject private ComBoardService cbsrv;
+	
+	@GetMapping("/test")
+	public void test() {
+		logger.debug(" test() 실행! ");
+	}
+	
+	@GetMapping("/add")
+	public void addGET() {
+		logger.debug(" /comboard/add -> addGET() 실행! ");
+	}
 	
 
 }
---
+++ JobGo/src/main/webapp/WEB-INF/views/comboard/add.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>커뮤니티 게시판 글쓰기</h1>
+	<fieldset>
+		<form action="/upload" method="post" enctype="multipart/form-data">
+			
+		</form>
+	</fieldset>
+</body>
+</html>
